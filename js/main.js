217e15f6598247e2187ce17a87b7276b15b73060
Rohan Mahato <rohankumarmahato@gmail.com>
2019-03-15 16:05:20 +0530

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -20,7 +20,14 @@ var time = 0;
 var tip = d3.tip()
 .attr("class", "d3-tip")
 .offset([-8, 0])
-.html(function(d) {  return "Continent : " + d.continent+"<br> Country : "+d.country+"<br/>Income : "+d.income +"<br> Population : "+d.population });
+.html(function(d) {  
+    var text = "<strong>Country : </strong> <span style='color:red'>" + d.country + "</span><br/>";
+    text += "<strong>Continent : </strong> <span style='color:red'>" + d.continent + "</span><br/>";
+    text += "<strong>Life Expectancy : </strong> <span style='color:red'>" + d.life_exp + "</span><br/>";
+    text += "<strong>GDP per Capita : </strong> <span style='color:red'>" + d.income + "</span><br/>";
+    text += "<strong>Population : </strong> <span style='color:red'>" + d.population + "</span><br/>";
+    return text;
+});
 
 g.call(tip);
 
